fcbffb757b08d031e0d20664663b35a915ae50c6
Kunlun Shen <kunlun_shen@hotmail.com>
2018-10-05 12:42:22 +1000

    test2

---
+++ GitTest2/src/GitDescp.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kunlun Shen
+ *
+ */
+public class GitDescp {
+
+	/**
+	 * 
+	 */
+	public GitDescp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
